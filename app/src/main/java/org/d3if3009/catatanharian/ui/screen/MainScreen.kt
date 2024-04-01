e82faa32ab439ffd3904ac6207999e646723e4e2
fahritech04 <fahricoding@gmail.com>
2024-04-01 11:28:51 +0800

    Membuat tampilan list

--- app/src/main/java/org/d3if3009/catatanharian/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3009/catatanharian/ui/screen/MainScreen.kt
@@ -1,7 +1,14 @@
 package org.d3if3009.catatanharian.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,8 +18,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if3009.catatanharian.R
+import org.d3if3009.catatanharian.model.Catatan
 import org.d3if3009.catatanharian.ui.theme.CatatanHarianTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,8 +50,40 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
 
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data) {
+            ListItem(catatan = it)
+            Divider()
+        }
+    }
 }
+
+@Composable
+fun ListItem(catatan: Catatan) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = catatan.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = catatan.catatan,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = catatan.tanggal)
+    }
+}
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
