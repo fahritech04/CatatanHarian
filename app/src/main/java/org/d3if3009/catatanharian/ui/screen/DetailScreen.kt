079b1f2262599a1be409766403ce23e0cf203c85
fahritech04 <fahricoding@gmail.com>
2024-04-01 16:54:50 +0800

    Meng-update data di database

--- app/src/main/java/org/d3if3009/catatanharian/ui/screen/DetailScreen.kt
+++ app/src/main/java/org/d3if3009/catatanharian/ui/screen/DetailScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -53,8 +54,9 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
     var judul by remember { mutableStateOf("") }
     var catatan by remember { mutableStateOf("") }
 
-    if (id != null) {
-        val data = viewModel.getCatatan(id)
+    LaunchedEffect(true) {
+        if (id == null) return@LaunchedEffect
+        val data = viewModel.getCatatan(id) ?: return@LaunchedEffect
         judul = data.judul
         catatan = data.catatan
     }
--- app/src/main/java/org/d3if3009/catatanharian/ui/screen/DetailViewModel.kt
+++ app/src/main/java/org/d3if3009/catatanharian/ui/screen/DetailViewModel.kt
@@ -25,4 +25,21 @@ class DetailViewModel(private val dao: CatatanDao) : ViewModel() {
             dao.insert(catatan)
         }
     }
+
+    suspend fun getCatatan(id: Long): Catatan? {
+        return dao.getCatatanById(id)
+    }
+
+    fun update(id: Long, judul: String, isi: String) {
+        val catatan = Catatan(
+            id      = id,
+            tanggal = formatter.format(Date()),
+            judul   = judul,
+            catatan = isi
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.update(catatan)
+        }
+    }
 }
